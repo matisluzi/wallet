f08e4ee66c8fc78bc74185b1e767da26474df838
Matis Luzi <matisluzi@gmail.com>
2019-09-18 17:45:09 +0200

    initial commit

---
+++ wallet/MasterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MasterViewController.swift
+//  wallet
+//
+//  Created by Matis Luzi on 9/18/19.
+//  Copyright © 2019 Matis Luzi. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ wallet/customCell.swift
@@ -0,0 +1,24 @@
+//
+//  customCell.swift
+//  wallet
+//
+//  Created by Matis Luzi on 9/18/19.
+//  Copyright © 2019 Matis Luzi. All rights reserved.
+//
+
+import UIKit
+
+class customCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
